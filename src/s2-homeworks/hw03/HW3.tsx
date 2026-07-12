27b79f9c3ff07014793182a31dfc2aa7bd3734c4
PavelKrevchik <pashakrevchik@yandex.by>
2026-07-12 17:01:38 +0300

    save-04

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -25,7 +25,7 @@ export type UserType = {
 
 export const pureAddUserCallback = (
    name: string, 
-   setUsers: React.Dispatch<React.SetStateAction<UserType[]>>, 
+   setUsers: (users: UserType[]) => void, 
    users: UserType[]
 ) => { // need to fix any
     const user = {
